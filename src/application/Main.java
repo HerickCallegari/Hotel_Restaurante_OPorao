7002d8a3cda8db4007e8c868520161427b31a9b3
Herick Campos <herickzeno@gmail.com>
2024-02-23 11:20:52 -0300

    troca de tela pronta

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	private static Scene mainScene;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			
+			mainScene = new Scene(parent);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Restaurante o Porão");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+}
